9f21f37954f78092d4bb5a4600c978aae9bfda60
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-12-17 08:41:30 +0000

    Use BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
 
 import ReactDOM from "react-dom/client";
-
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  
+  <BrowserRouter basename={import.meta.env.DEV ? "/" : "/ReactApp1/"}>
     <AppRoutes />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );

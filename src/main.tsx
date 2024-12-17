90ef6bd8ce4d0da68533968272f1018e37641661
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-12-17 08:27:14 +0000

    edit main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+
+import AppRoutes from "./AppRoutes.tsx";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>,
+);

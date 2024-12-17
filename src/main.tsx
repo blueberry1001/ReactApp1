5b8481628708f39aa30098483429e0aae994f142
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-12-17 08:52:36 +0000

    delete browserrouter settings

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import AppRoutes from "./AppRoutes.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
-  <BrowserRouter basename={import.meta.env.DEV ? "/" : "/ReactApp1/"}>
+  <BrowserRouter>
     <AppRoutes />
   </BrowserRouter>,
 );

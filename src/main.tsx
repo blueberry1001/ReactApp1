3989b16c54ecd43c8db182a6e654889041a76f3d
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-12-17 09:07:18 +0000

    don't use brouserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 
-  <BrowserRouter>
-    <div><a>あっはっは！</a></div>
+  <React.StrictMode>
     <AppRoutes />
-  </BrowserRouter>,
+  </React.StrictMode>,
 );

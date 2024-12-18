1b7f1ff92acff7623a5ff1f861019e3fb4cad881
Blueberry <98882175+blueberry1001@users.noreply.github.com>
2024-12-18 00:54:37 +0000

    Delete All and Create いろいろ

---
+++ reactailwind/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.GITHUB_PAGES  // この行を追加
+        ? "ReactApp1"            // この行を追加
+        : "./",                     // この行を追加
+  plugins: [react()],
+})
